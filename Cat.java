bf4a4adfbbb418443615ff50aee358c4f452ad07
christian <cdirubbs@gmail.com>
2024-03-07 17:12:45 -0500

    ut 10 nw

--- Cat.java
+++ Cat.java
@@ -1,23 +1,21 @@
 import java.lang.Math;
+
 public class Cat extends Creature {
 
-    
+    // dir: 0=North, 1=East, 2=South, 3=West.
+    //
+    //
+    //
+    // N (r-1,c+0)
+    // 0
+    // (r+0,c-1) W 3 [ ] 1 E (r+0,c+1)
+    // 2
+    // S (r+1,c+0)
+    //
+    //
+    //
+    //
 
-
-      // dir: 0=North, 1=East, 2=South, 3=West.
-    // 
-    //
-    //
-    //               N (r-1,c+0)
-    //               0
-    //(r+0,c-1) W 3 [ ]  1 E (r+0,c+1)
-    //               2
-    //               S (r+1,c+0)
-    //
-    //
-    // 
-    // 
-    
     private int movesSinceMouseEaten = 1;
     private Mouse targetMouse;
 
@@ -27,68 +25,50 @@ public class Cat extends Creature {
         this.lab = LAB_YELLOW;
     }
 
-    public void takeAction(){
-        if (dead == true) return;  
+    public void takeAction() {
+        if (dead == true)
+            return;
         checkToEat();
-   
         findClosestMouse();
+
         if (targetMouse != null) {
             dirToTarget(targetMouse);
         }
-        // if (findClosestMouse()) {
-        //     // set target, move to target
-        //     dirToTarget(targetMouse);
-        //     super.step();
-        //     movesSinceMouseEaten++;
-        //     return;
-        // }
-
 
     }
 
-
-
-     public void step(){
-        movesSinceMouseEaten++;
+    public void step() {
         killCatMaybe();
         maybeTurn();
-        super.step();     
-
+        movesSinceMouseEaten++;
+        super.step();
     }
 
-
-
-    private boolean findClosestMouse() {;
+    private boolean findClosestMouse() {
         int targetDist = Integer.MAX_VALUE;
+        boolean mouseFound = false;
         for (Creature c : city.creatures) {
             if (mouseInDistance(c) != -1 && mouseInDistance(c) < targetDist && (c instanceof Mouse)) {
                 setTargetMouse(c);
                 targetDist = mouseInDistance(c);
-                
+                mouseFound = true;
             }
-         }
-         if (targetDist < Integer.MAX_VALUE) {
+        }
+        if (mouseFound) {
             return true;
-         }
-         else {
+        } else {
             setTargetMouse(null);
             return false;
-         }
-
-
-         // return true if mouse is found, false if mouse isnt found
-
-        // set target, move to target
+        }
     }
 
     private int mouseInDistance(Creature c) {
         if (this.getGridPoint().dist(c.getGridPoint()) < 20 && (c instanceof Mouse)) {
             return this.getGridPoint().dist(c.getGridPoint());
-        }
-        else {
+        } else {
             return -1;
         }
-        
+
     }
 
     private void setTargetMouse(Creature c) {
@@ -106,51 +86,46 @@ public class Cat extends Creature {
     }
 
     private void dirToTarget(Mouse target) {
-            int vertDist = Math.abs(this.getY() - target.getY());
-            int horizDist = Math.abs(this.getX() - target.getX());
-            // if vertical distance strictly greater than horiz
-            if (vertDist > horizDist) {
-                if (target.getY() <= this.getY()) {
-                    this.setDir(0);
-                    return;
-                } 
-                else {
-                    this.setDir(2);
-                    return;
-                }
+        int vertDist = Math.abs(this.getY() - target.getY());
+        int horizDist = Math.abs(this.getX() - target.getX());
+        // if vertical distance strictly greater than horiz
+
+        if (horizDist < vertDist) {
+            if (target.getY() < this.getY()) {
+                this.setDir(0);
+                return;
+            } else {
+                this.setDir(2);
+                return;
             }
-            else {
-                if (this.getX() <= target.getX()) {
-                    this.setDir(1);
-                    return;
-                }
-                else {
-                    this.setDir(3);
-                    return;
-                }
+        } else {
+            if (this.getX() < target.getX()) {
+                this.setDir(1);
+                return;
+            } else {
+                this.setDir(3);
+                return;
             }
         }
-
+    }
 
     private void killCatMaybe() {
         // cat hasnt eaten mouse in 50 moves
-        if (movesSinceMouseEaten == 51) {
+        if (movesSinceMouseEaten == 50) {
             this.dead = true;
         }
     }
 
     private void checkToEat() {
         // Check if any mouse is at current x and y, to EAT
-            if (targetMouse == null) {
-                return;
-            }
-            if (this.getGridPoint().equals(targetMouse.getGridPoint())) {
-                targetMouse.dead = true;
-                movesSinceMouseEaten = 0;
-                setTargetMouse(null);
-                System.err.println("mouse eaten");
-                return;
-            }
+        if (targetMouse == null) {
+            return;
+        }
+        if (this.getGridPoint().equals(targetMouse.getGridPoint())) {
+            targetMouse.dead = true;
+            movesSinceMouseEaten = 0;
+            return;
+        }
     }
 
     private void maybeTurn() {
@@ -161,5 +136,3 @@ public class Cat extends Creature {
     }
 
 }
-
-
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # cdirubbio-project1
-  UT failures 8,9,10
 ## Simulator
 
 
@@ -9,10 +8,3 @@
 ## The Cat class 
 
 
-  In the step method: only die, update any counters associated with dying/lifespan, and randomly turn. Remember that in the simulator step will always be called before takeAction.
-
-  In the takeAction method: you'll want to do anything associated with chasing (if a Cat), or adding the mouse (if a Mouse).
-
-  Basically, make sure your step is not doing more that updating the life of the creature and doing a naive move. Because takeAction is called after step in the simulator, for cats that need to chase, the direction will be updated in the takeAction method if they need to chase, overwriting any direction update in step.
-
-  Also, check your output for negative coordinates; not only will these not plot, but you'll want to wrap the negative coordinates around the board. Hint: use modulus with WIDTH or HEIGHT in this expression.
